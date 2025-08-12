4411f82f1d01ac1966d04a666a0c49114f1b345e
SriramVijayan <raul_sriram@hotmail.com>
2025-08-12 08:53:53 -0400

    Adjusted Plants text to center of nav bar

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -237,6 +237,11 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
+    const plantsTextStyle = {
+        alignItems: 'center',
+        paddingLeft: '300px',
+    }
+
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -295,7 +300,7 @@ function ProductList({ onHomeClick }) {
 
                 </div>
                 <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={{...styleA, ...plantsTextStyle}}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path><text dominantBaseline="middle" textAnchor="middle" fill="white" x="50%" y="50%" fontSize="80" >{calculateTotalQuantity()}</text></svg></h1></a></div>                                                            
                 </div>
             </div>
@@ -321,7 +326,6 @@ function ProductList({ onHomeClick }) {
                                         className = {checkPlantInCart(plant) ? "product-button added-to-cart" : "product-button"}
                                         onClick={() => handleAddToCart(plant)}
                                         disabled = {checkPlantInCart(plant)}
-                                        //disabled={addedToCart[plant.name]}                                        
                                     >
                                         {checkPlantInCart(plant) ? 'Added to Cart' : 'Add to Cart'}                                        
                                     </button>
